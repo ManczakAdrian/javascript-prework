9f3c09b0dda64e6deb1987240718ab7778cbcdd7
Adrian Manczak <manczakadrian@gmail.com>
2023-04-08 12:42:49 +0200

    variable use

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 printMessage('Zagrałem kamieńx! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `nożyce`;
+let playerMove=`papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
